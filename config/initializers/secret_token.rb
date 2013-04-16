f87cb890811bc2626423a1db66cd1509a1f9b4fc
Kensuke Nagae <kyanny@gmail.com>
2013-04-16 16:35:15 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ChromeExtensionUpdateCenter::Application.config.secret_token = '5e9471bd76bdc6a3dfecfc74665fb0bac79a96531d6d3025e1db62f8164eb46d1e8e327a060ee35f28742c241fdac7423098ddb77ba85406bbf61e42c3fb95fd'
